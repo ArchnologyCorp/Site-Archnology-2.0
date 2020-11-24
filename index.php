7a75623c936a4766b65f1ff52b9a7ac3671b8d4c
Leonardo <leonardoiigd2013@gmail.com>
2020-11-24 19:10:55 -0300

    adicionando navbar no site

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title> Archnology Corp </title>
+	<meta name="viewport" content="width=device-width,initial-scale=1">
+	<!-- CSS -->
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="css/estilo.css">
+</head>
+<body>
+	<nav class="navbar navbar-dark" style="background-color: #000;" >
+		<div class="container-fluid">
+			<div class="navbar-header">
+				<img src="img/logo.png" class="d-inline-block align-top" style="width:40%; height:60%"></img>
+			</div>
+			<div class="navbar-brand navbar-right">
+				<ul class="nav justify-content-center">
+					<li class="nav-item">
+						<img class="nav-link" src="img/brasil.png" class="d-inline-block align-top" style="float: right; padding-right:5%; padding-bottom:5%; max-width: 11%;"></img>
+					</li>
+					<li> <a href="#" style="color: white">
+						<svg width="2em" height="2em" viewBox="0 0 16 16" class="bi bi-box-arrow-right" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+						<path fill-rule="evenodd" d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0v2z"/>
+						<path fill-rule="evenodd" d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z"/>
+						</svg>
+					</li></a>
+				</ul>
+			</div>
+				<ul class="navbar-dark nav" style="margin-left: 30%; font-size: 140%;">
+					<li class="nav-item active">
+						<a class="nav-link" href="index.php" style="color: white">Home</a>
+					</li>
+					<li class="nav-item active">
+						<a class="nav-link" href="#" style="color: white">Empresa</a>
+					</li>
+					<li class="nav-item active">
+						<a class="nav-link" href="#" style="color: white">Portfólio</a>
+					</li>
+					<li class="nav-item active">
+						<a class="nav-link" href="#" style="color: white">Contato</a>
+					</li>
+					<li class="nav-item active">
+						<a class="nav-link" href="#" style="color: white">Suporte</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</nav>
+</body>
+</html>
